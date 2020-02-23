26f499565a5bfe5817407f83ce4ef568d5663e23
Rahul raj <42221167+Rahulrajsit@users.noreply.github.com>
2020-02-23 18:32:58 +0530

    Update GroupAnagrams.java

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,120 +1,64 @@
 package HashMap_List;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Random;
 
-public class GroupAnagrams {
+public class P3_Distinct_Anagram_Class {
 
-	public static boolean checkAnagram(String s1, String s2) {
-		if (s1.length() != s2.length())
-			return false;
-		int[] count = new int[26];
-		for (int i = 0; i < s1.length(); ++i)
-			++count[s1.charAt(i) - 'a'];
-		for (int i = 0; i < s2.length(); ++i) {
-			if (count[s2.charAt(i) - 'a'] == 0)
-				return false;
-			--count[s2.charAt(i) - 'a'];
-		}
-		return true;
+	public static String[] Testcase1(int x) {
+		Random r = new Random();
+		String[] str = new String[x];
+		for (int i = 0; i < x; i++)
+			str[i] = "str" + r.nextInt(5);
+		return str;
 	}
 
-	public static void groupAnagrams1(String[] in) {
-		boolean[] used = new boolean[in.length];
-		for (int i = 0; i < in.length; ++i) {
-			if (used[i])
-				continue;
-			System.out.println(in[i]);
-			for (int j = i + 1; j < in.length; ++j) {
-				if (checkAnagram(in[i], in[j])) {
-					System.out.println(in[j]);
-					used[j] = true;
-				}
-			}
-			System.out.println();
-		}
-	}
-
-	static class Pair {
-		String first;
-		String second;
-
-		public Pair(String first, String second) {
-			this.first = first;
-			this.second = second;
-		}
-
-		@Override
-		public String toString() {
-			return "Pair [first=" + first + ", second=" + second + "]";
-		}
-
-	}
-
-	static class MyComparator implements Comparator<Pair> {
-
-		@Override
-		public int compare(Pair o1, Pair o2) {
-			return o1.first.compareTo(o2.first);
-		}
-
-	}
-
-	public static void groupAnagrams2(String[] in) {
-		Pair[] aux = new Pair[in.length];
-		for (int i = 0; i < in.length; ++i) {
-			char[] tmp = in[i].toCharArray();
-			Arrays.parallelSort(tmp);
-			String sorted_string = String.valueOf(tmp);
-			aux[i] = new Pair(sorted_string, in[i]);
-		}
-		Arrays.parallelSort(aux, new MyComparator());
-		for (Pair tmp : aux)
-			System.out.println(tmp);
-	}
-
-	public static void groupAnagrams3(String[] in) {
-		HashMap<String, ArrayList<String>> hmap = new HashMap<String, ArrayList<String>>();
+	public static int AnagramClass(String[] in) {
+		int incount = 1;
 		for (String s : in) {
-			char[] tmp = s.toCharArray();
-			Arrays.parallelSort(tmp);
-			String sorted_string = String.valueOf(tmp);
-			if (hmap.containsKey(sorted_string)) {
-				hmap.get(sorted_string).add(s);
-			} else {
-				ArrayList<String> list = new ArrayList<String>();
-				list.add(s);
-				hmap.put(sorted_string, list);
-			}
+			s.toCharArray();
+			System.out.println(s.toCharArray());
 		}
-		for (String key : hmap.keySet()) {
-			System.out.println(key + ":" + hmap.get(key));
+		for (int i = 0; i < in.length - 1; i++) {
+			if (!in[i].equals(in[i + 1]))
+				incount++;
 		}
+		return incount;
 	}
 
-	public static String[] testCase1(int n, int k) {
-		String[] in = new String[n];
-		Random r = new Random(0);
-		for (int i = 0; i < n; ++i) {
-			String tmp = "";
-			for (int j = 0; j < k; ++j)
-				tmp += (char) ('a' + r.nextInt(26));
-			in[i] = tmp;
+	public static void AnagramClass3(String[] in) {
+		HashMap<String, List<String>> hmap = new HashMap<String, List<String>>();
+		for (int i = 0; i < in.length; ++i) {
+			char[] tmp1 = in[i].toCharArray();
+			Arrays.sort(tmp1);
+			String tmp2 = Arrays.toString(tmp1);
+			List<String> val = hmap.get(tmp2);
+			if (val == null) {
+				ArrayList<String> list = new ArrayList<String>();
+				list.add(in[i]);
+				hmap.put(tmp2, list);
+			} else
+				val.add(in[i]);
 		}
-		return in;
+		for (String tmp : hmap.keySet())
+			System.out.println(tmp + ":" + hmap.get(tmp));
+		System.out.println("Total Distinct Anagram is:" + hmap.size());
 	}
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int n = Integer.parseInt(args[0]);
-		int k = Integer.parseInt(args[1]);
-		String[] in = testCase1(n, k);
-		for (String tmp : in)
-			System.out.print(tmp + ",");
-		System.out.println();
-		groupAnagrams1(in);
+		String[] in = Testcase1(n);
+		System.out.println(Arrays.toString(in));
+
+		long start = System.currentTimeMillis();
+		AnagramClass3(in);
+		long end = System.currentTimeMillis();
+		System.out.println("Time taken:" + (end - start) / 1000.0 + " secs");
+
 	}
 
 }
